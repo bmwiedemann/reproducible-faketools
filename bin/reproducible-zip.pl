1541d67e669e54954e5359a4550fe3a03e16255b
Bernhard M. Wiedemann <bwiedemann@suse.de>
2017-06-21 15:29:05 +0200

    zip: some more incomplete mappings

--- bin/reproducible-zip.pl
+++ bin/reproducible-zip.pl
@@ -5,7 +5,7 @@ Getopt::Long::Configure ("bundling");
 my @mappedargv=qw(7z -tzip -mtc=off);
 my @lateargv=();
 my %options=qw();
-my @options=qw"recurse-paths|r filesync delete|d update|u test|T junk-paths|j quiet|q names-stdin|@ 0 1 2 3 4 5 6 7 8 9";
+my @options=qw"recurse-paths|r filesync delete|d update|u test|T move|m exclude|x=s no-extra|X no-dir-entries|D junk-paths|j quiet|q names-stdin|@ 0 1 2 3 4 5 6 7 8 9";
 GetOptions(\%options, @options);
 #print (%options);
 
@@ -13,6 +13,7 @@ for(0..9) {
     if($options{$_}) {push(@mappedargv, "-mx=$_")}
 }
 push(@mappedargv, "-bd") if $options{quiet};
+push(@mappedargv, "-x".$options{exclude}) if $options{exclude};
 if($options{"junk-paths"}) { die "junk-paths option is not supported in $0" }
 if($options{"names-stdin"}) {push(@lateargv, '@/dev/stdin')}
 
@@ -25,6 +26,7 @@ my %actionmap=qw(
 if($actionmap{$ARGV[0]}) {
     $action=$actionmap{shift(@ARGV)};
 }
+if($options{move}) {$action="a"}
 if($options{update}) {$action="u"}
 if($options{delete}) {$action="d"}
 if($options{test}) {$action="t"}
