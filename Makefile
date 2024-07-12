8f6777bf9389066f972cd71c4d728841052cd360
Bernhard M. Wiedemann <bwiedemann@suse.de>
2024-07-12 20:01:57 +0200

    Add tests

--- Makefile
+++ Makefile
@@ -4,3 +4,9 @@ install:
 	install -p -m 755 bin/* $(DESTDIR)$(PREFIX)/bin/
 	cp -a lib/reproducible-faketools $(DESTDIR)$(PREFIX)/lib/
 	install -p -m 644 extra/disorderfs.sh $(DESTDIR)/etc/profile.d/
+
+roundup:
+	git clone https://github.com/SUSE-Cloud/roundup
+
+test: roundup
+	cd tests && ../roundup/roundup.sh
---
+++ tests/jar-test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env roundup
+
+describe "roundup(1) testing of jar"
+
+it_normalizes_mtime_on_create() {
+   mkdir -p tmp
+   echo foo > tmp/foo
+   SOURCE_DATE_EPOCH=1 ../bin/jar --create --file tmp/test.jar tmp/foo
+   hash=$(md5sum tmp/test.jar | cut -d" " -f1)
+   rm -rf tmp
+   [[ "$hash" = 5c9de655dea174b60f195771ce3094fa ]] || [[ "$hash" = 7eebf70ccec32147cd291da1b3acb041 ]]
+}
